ca7f0fa156948df85d6513516a4dd018a211265f
TheBusyBiscuit <mrCookieSlime@gmail.com>
2017-03-10 15:25:11 +0100

    Updated script

--- batch/Download & Run.bat
+++ batch/Download & Run.bat
@@ -3,9 +3,11 @@
 if not exist "%cd%/JAFileMinifier" (
 	mkdir "%cd%/JAFileMinifier"
 )
-	
-bitsadmin.exe /transfer "Jar Download" https://github.com/TheBusyBiscuit/JAFileMinifier/raw/master/dist/JAFileMinifier.jar "%cd%/JAFileMinifier/JAFileMinifier.jar"
 
+if not exist "%cd%/JAFileMinifier/JAFileMinifier.jar" (
+	bitsadmin.exe /transfer "Jar Download" https://github.com/TheBusyBiscuit/JAFileMinifier/raw/master/dist/JAFileMinifier.jar "%cd%/JAFileMinifier/JAFileMinifier.jar"
+)
+	
 for /d %%a in ("%cd%\*") do (
     echo Directory %%~nxa
 	java -jar JAFileMinifier/JAFileMinifier.jar %%~nxa
